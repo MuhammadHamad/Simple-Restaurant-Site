5715442951a83c6ffa0743f9b8c104f91f3d8530
Muhammad Hammad <hamaadkhan066@gmail.com>
2022-04-26 18:22:20 +0500

    "Solve minor errors"

--- src/components/basics/Restaurant.js
+++ src/components/basics/Restaurant.js
@@ -1,25 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuCard from "./MenuCard";
+import Menu from "./menuapi";
 import "./style.css";
 
 const Restaurant = () => {
+  const [menuData, setMenuData] = useState(Menu);
   return (
     <>
-      <div className="card-container">
-        <div className="card">
-          <div className="card-body">
-            <span className="card-number card-circle subtle">1</span>
-            <span className="card-author subtle">Dinner</span>
-            <h2 className="card-title">Maggi</h2>
-            <span className="card-description subtle">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aut
-              maxime, cum repellendus molestias ratione asperiores vitae
-              inventore culpa iure.
-            </span>
-            <div className="card-read">Read</div>
-          </div>
-          <img src="" alt="images" className="card-media"/>
-        </div>
-      </div>
+      <MenuCard menuData={menuData}/>
     </>
   );
 };
