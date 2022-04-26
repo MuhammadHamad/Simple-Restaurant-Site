57e666b05757c37e4eeb75ba041e2aa53ffac9ed
Muhammad Hammad <hamaadkhan066@gmail.com>
2022-04-26 06:17:03 +0500

    first commit

---
+++ src/components/basics/Restaurant.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+
+const Restaurant = () => {
+  return (
+    <>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-body">
+            <span className="card-number card-circle subtle">1</span>
+            <span className="card-author subtle">Breakfast</span>
+            <h2 className="card-title">Maggi</h2>
+            <span className="card-description subtle">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum aut
+              maxime, cum repellendus molestias ratione asperiores vitae
+              inventore culpa iure.
+            </span>
+            <div className="card-read">Read</div>
+          </div>
+          <img src="" alt="images" className="card-media"/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Restaurant;
